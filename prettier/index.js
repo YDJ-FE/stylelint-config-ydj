c5db71d3c63607cd4aedc9bca7830c24d681e743
vegawong <vegawong1024@gmail.com>
2018-02-07 11:24:27 +0800

    up 添加prettier节点

---
+++ prettier/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+// @ts-nocheck
+
+module.exports = {
+    ignoreFiles: ["node_modules"],
+    extends: ['stylelint-config-prettier'],    
+    rules: {
+        'at-rule-empty-line-before': null,
+        'selector-pseudo-element-colon-notation': null,
+        'selector-combinator-space-before': null,
+        'selector-combinator-space-after': null,
+        'no-missing-end-of-source-newline': null
+    }
+  };
+  
